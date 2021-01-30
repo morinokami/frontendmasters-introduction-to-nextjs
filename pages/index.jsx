a399f9ab418b76cad01595e13be49cdc7666de18
Shinya Fujino <shf0811@gmail.com>
2021-01-30 19:55:58 +0900

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+export default ({ content }) => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: 'containers.page',
+        display: 'flex',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
+    </div>
+  </div>
+)
+
+export function getStaticProps() {
+  // get data from CMS
+
+  return {
+    props: {
+      content: {
+        title: 'This is my really nice app',
+      },
+    },
+  }
+}
---
+++ src/data/data.js
@@ -0,0 +1,23 @@
+// const notes = new Array(15).fill(1).map((_, i) => ({
+//   id: Date.now() + i,
+//   title: `Note ${i}`,
+// }))
+const notes = [
+  { id: 1612001130805, title: 'Note 0' },
+  { id: 1612001130806, title: 'Note 1' },
+  { id: 1612001130807, title: 'Note 2' },
+  { id: 1612001130808, title: 'Note 3' },
+  { id: 1612001130809, title: 'Note 4' },
+  { id: 1612001130810, title: 'Note 5' },
+  { id: 1612001130811, title: 'Note 6' },
+  { id: 1612001130812, title: 'Note 7' },
+  { id: 1612001130813, title: 'Note 8' },
+  { id: 1612001130814, title: 'Note 9' },
+  { id: 1612001130815, title: 'Note 10' },
+  { id: 1612001130816, title: 'Note 11' },
+  { id: 1612001130817, title: 'Note 12' },
+  { id: 1612001130818, title: 'Note 13' },
+  { id: 1612001130819, title: 'Note 14' },
+]
+
+module.exports = notes
